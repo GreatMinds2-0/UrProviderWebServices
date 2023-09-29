363b0e1e18f7bf88293f237f898dcebd78aea8fb
Wilfredo Futuri Illa <u201924361@upc.edu.pe>
2023-09-29 00:43:29 -0500

    feat: modified cors

--- src/main/java/com/acme/urproviderwebservices/UrProviderWebServicesApplication.java
+++ src/main/java/com/acme/urproviderwebservices/UrProviderWebServicesApplication.java
@@ -32,7 +32,7 @@ public class UrProviderWebServicesApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://urprovider.herokuapp.com/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://hesitant-turn-production.up.railway.app/swagger-ui/index.html"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
