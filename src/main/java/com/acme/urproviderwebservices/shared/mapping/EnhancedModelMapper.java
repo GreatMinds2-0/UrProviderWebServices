db3853469902dee93d687894d13572acfff9f6b3
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-09-29 15:27:07 -0500

    update: Enhanced model mapper updated

--- src/main/java/com/acme/urproviderwebservices/shared/mapping/EnhancedModelMapper.java
+++ src/main/java/com/acme/urproviderwebservices/shared/mapping/EnhancedModelMapper.java
@@ -7,10 +7,5 @@ import java.util.stream.Collectors;
 
 public class EnhancedModelMapper extends ModelMapper {
 
-    public EnhancedModelMapper() { super(); }
 
-    public <S, T> List<T> mapList(List<S> sourceList, Class<T> targetClass) {
-        return sourceList.stream().map(item -> this.map(item, targetClass))
-                .collect(Collectors.toList());
-    }
 }
