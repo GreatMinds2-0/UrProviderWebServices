4ffa445235e1a6abc9a2613ce959c45d307a1788
Frank_Alva <u202020026@upc.edu.pe>
2023-09-29 01:01:18 -0500

    feat: fix url cors

--- src/main/java/com/acme/urproviderwebservices/UrProviderWebServicesApplication.java
+++ src/main/java/com/acme/urproviderwebservices/UrProviderWebServicesApplication.java
@@ -32,15 +32,17 @@ public class UrProviderWebServicesApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://hesitant-turn-production.up.railway.app/swagger-ui/index.html"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://hesitant-turn-production.up.railway.app/swagger-ui/index.html"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
                 "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
+
     }
 }
